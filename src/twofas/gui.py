6c3c8000371cd7ed8e4d7e381a54a1d58b30eaf6
Robin van der Noord <robinvandernoord@gmail.com>
2024-02-06 10:25:23 +0100

    refactor: use screeninfo instead of tkinter, extracted threading logic to 'threadful'

--- src/twofas/gui.py
+++ src/twofas/gui.py
@@ -1,7 +1,6 @@
 import base64
 import json
 import sys
-import threading
 import types
 import typing
 import warnings
@@ -12,6 +11,7 @@ import requests
 
 try:
     from result import Err, Ok, Result
+    from threadful import ThreadWithReturn, thread
 except ImportError:
     # when the [gui] extra is not installed
     # an error will be thrown later.
@@ -36,94 +36,13 @@ WEB_DIR = Path(__file__).parent / "web"
 CACHE_DIR = Path("~/.cache").expanduser() / "2fas"
 ICON_DIR = CACHE_DIR / "icons"
 
-P = typing.ParamSpec("P")
-R = typing.TypeVar("R")
-
-
-class ThreadWithReturn(typing.Generic[R], threading.Thread):
-    _target: typing.Callable[P, R]
-    _args: P.args
-    _kwargs: P.kwargs
-    _return: R
-
-    def __init__(self, target: typing.Callable[P, R], *a: typing.Any, **kw: typing.Any) -> None:
-        kw["target"] = target
-        super().__init__(*a, **kw)
-
-    def run(self) -> None:
-        try:
-            if self._target is not None:
-                self._return = self._target(*self._args, **self._kwargs)
-        finally:
-            # Avoid a refcycle if the thread is running a function with
-            # an argument that has a member that points to the thread.
-            del self._target, self._args, self._kwargs
-
-    def result(self) -> "Result[R, None]":
-        if self.is_alive():
-            # still busy
-            return Err(None)
-        else:
-            return Ok(self._return)
-
-
-@typing.overload
-def thread(my_function: typing.Callable[P, R]) -> typing.Callable[P, ThreadWithReturn[R]]:
-    # code in this function is never executed, just shown for reference of the complex return type
-
-    def wraps(*a: P.args, **kw: P.kwargs) -> ThreadWithReturn[R]:
-        my_thread = ThreadWithReturn(target=my_function, args=a, kwargs=kw)
-        my_thread.start()
-        return my_thread
-
-    return wraps
-
-
-@typing.overload
-def thread(
-    my_function: None = None,
-) -> typing.Callable[[typing.Callable[P, R]], typing.Callable[P, ThreadWithReturn[R]]]:
-    # code in this function is never executed, just shown for reference of the complex return type
-
-    def wraps(inner_function: typing.Callable[P, R]) -> typing.Callable[P, ThreadWithReturn[R]]:
-        def inner(*a: P.args, **kw: P.kwargs) -> ThreadWithReturn[R]:
-            my_thread = ThreadWithReturn(target=inner_function, args=a, kwargs=kw)
-            my_thread.start()
-            return my_thread
-
-        return inner
-
-    return wraps
-
-
-def thread(
-    my_function: typing.Callable[P, R] | None = None
-) -> (
-    typing.Callable[[typing.Callable[P, R]], typing.Callable[P, ThreadWithReturn[R]]]
-    | typing.Callable[P, ThreadWithReturn[R]]
-):
-    # decorator
-
-    if my_function is not None:
-
-        def wraps(*a: P.args, **kw: P.kwargs) -> ThreadWithReturn[R]:
-            my_thread = ThreadWithReturn(target=my_function, args=a, kwargs=kw)
-            my_thread.start()
-            return my_thread
-
-        return wraps
-
-    else:
-        return thread
-
 
 def get_screen_width() -> tuple[int, int]:
-    import tkinter
+    from screeninfo import get_monitors
 
-    root = tkinter.Tk()
-    width = root.winfo_screenwidth()
-    height = root.winfo_screenheight()
-    root.destroy()  # Close the Tkinter window
+    monitors = get_monitors()
+    width = monitors[0].width
+    height = monitors[0].height
     return width, height
 
 
